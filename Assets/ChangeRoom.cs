226cc490f943c68de19a93c6d2e98d88aecd977f
zweetdreams <benjaminnettervik@hotmail.com>
2018-10-01 15:45:14 +0200

    Rumbyte och början på effekter

---
+++ Assets/ChangeRoom.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeRoom : MonoBehaviour
+{
+    //Denna ska endas assignas om det är ett rum spelaren spawnar i
+    public GameObject startingRoom;
+
+    public GameObject roomToDisable;
+    public GameObject roomToEnable;
+
+    private void Start()
+    {
+        if (roomToEnable != startingRoom)
+        {
+            ChangeRoomState(roomToEnable, false);
+
+        }
+    }
+
+    bool hasEntered;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!hasEntered)
+        {
+            hasEntered = true;
+
+            if (other.name == "Player")
+            {
+                ChangeRoomState(roomToEnable, true);
+                ChangeRoomState(roomToDisable, false);
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.name == "Player")
+        {
+            hasEntered = false;
+
+        }
+    }
+
+    void ChangeRoomState(GameObject room, bool state)
+    {
+        foreach (Transform objectInRoom in room.transform)
+        {
+            if (objectInRoom.GetComponent<MeshRenderer>() != null)
+            {
+                objectInRoom.GetComponent<MeshRenderer>().enabled = state;
+            }
+        }
+    }
+}
---
+++ Assets/MovementPlaceholder.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementPlaceholder : MonoBehaviour
+{
+    private void Update()
+    {
+
+        transform.position += new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")) * Time.deltaTime * 5;
+    }
+}
