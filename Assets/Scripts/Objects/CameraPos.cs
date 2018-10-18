05eee2c48537de558c8e825abf8c568c36034594
zweetdrmz <benjaminnettervik@hotmail.com>
2018-10-18 15:01:59 +0200

    Helt fungerande movement och mouselook

---
+++ Assets/Scripts/Objects/CameraPos.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraPos : MonoBehaviour
+{
+    GameObject player;
+
+    private void Awake()
+    {
+        player = GameObject.Find("Player");
+    }
+
+    private void Update()
+    {
+        transform.position = player.transform.position + new Vector3(-8, 10, 2);
+    }
+}
