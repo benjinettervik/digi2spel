acb64c6c21d8476b51cc818d1d080d8c589fa32a
zweetdreams <benjaminnettervik@hotmail.com>
2018-11-12 15:41:22 +0100

    Placeholder sword animation

---
+++ Assets/Sword.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sword : MonoBehaviour
+{
+
+    Animator animationController;
+
+    private void Awake()
+    {
+        animationController = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        Attack();
+    }
+
+    void Attack()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            animationController.Play("Swordplaceholder");
+        } 
+    }
+
+}
