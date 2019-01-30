e1bfb2a6d88389fa2f2f005c27ab93c44e5761e4
zweetdrmz <benjaminnettervik@hotmail.com>
2019-01-30 10:32:13 +0100

    navmesh

---
+++ Assets/Scripts/enemy/Enemy.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour
+{
+    [HideInInspector]
+    public NavMeshAgent agent;
+    public float health;
+    public float damage;
+    public float sightRange;
+    public float engagementDistance;
+    [HideInInspector]
+    public bool playerIsSpotted;
+    [HideInInspector]
+    public Vector3 playerLastSpotted = Vector3.zero;
+
+    [HideInInspector]
+    public GameObject player;
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    public void TakeDamage(float damage)
+    {
+        health -= damage;
+
+        if (health <= 0)
+        {
+            //die
+        }
+    }
+
+    public void SpotPlayer()
+    {
+        if (Vector3.Distance(transform.position, player.transform.position) < sightRange)
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position, player.transform.position - transform.position, out hit, sightRange))
+            {
+                if (hit.collider.tag == "Player")
+                {
+                    playerLastSpotted = hit.collider.transform.position;
+                    print("player is spotted");
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/enemy/MeleeEnemy.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeleeEnemy : Enemy
+{
+    float timeSinceLastAttack;
+    bool isAttacking;
+    public float attackSpeed;
+
+    private void Update()
+    {
+        SpotPlayer();
+        if (playerLastSpotted != Vector3.zero)
+        {
+            agent.SetDestination(playerLastSpotted);
+        }
+
+        print(agent.isStopped);
+
+        MeleeAttack();
+    }
+
+    void MeleeAttack()
+    {
+        timeSinceLastAttack += Time.deltaTime;
+        //print(timeSinceLastAttack);
+        if (Vector3.Distance(transform.position, player.transform.position) < engagementDistance)
+        {
+            agent.isStopped = true;
+            agent.velocity *= 0.95f;
+            isAttacking = true;
+            if (timeSinceLastAttack > attackSpeed)
+            {
+                timeSinceLastAttack = 0;
+                //print("attacking");
+            }
+        }
+        else
+        {
+            print("stopping attacking");
+            isAttacking = false;
+            agent.isStopped = false;
+        }
+    }
+}
